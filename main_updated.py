11413cf762014d85382909c8706ceee758fa238b
Adorján <65911017+adorjannm@users.noreply.github.com>
2025-11-10 18:11:19 +0100

    Add Hydra Config System Fixes adorjannm/student-search#2

--- main_updated.py
+++ main_updated.py
@@ -1,10 +1,11 @@
-import argparse
 from pettingzoo.mpe._mpe_utils.simple_env import make_env
 
 from sar_env_updated import make_env, raw_env, parallel_wrapper_fn, parallel_env, env
 from pettingzoo.test import parallel_api_test
 import supersuit as ss
 
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
 import glob
 import os
@@ -17,37 +18,37 @@ from stable_baselines3 import PPO
 from stable_baselines3.ppo import CnnPolicy, MlpPolicy
 
 from train_updated import train
-# from sb3_contrib import RecurrentPPO
-# from stable_baselines3.common.evaluation import evaluate_policy
-# from stable_baselines3 import DQN
-# from stable_baselines3.dqn import CnnPolicy, MlpPolicy
 
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Image captioning training pipeline")
-    parser.add_argument(
-        "--train",
-        action="store_true",
-        help="Run training for the search and rescue environment.",
-    )
-    parser.add_argument(
-        "--eval",
-        action="store_true",
-        help="Evaluate a trained model in the search and rescue environment with graphical visualization.",
-    )
-    # parse_known_args avoids crashing when extra arguments are forwarded
-    args, _ = parser.parse_known_args()
-    return args
 
-def main():
-    # Set render_mode to None to reduce training time
-    env_kwargs = dict(num_missing=1, num_rescuers=3, num_trees=8, num_safezones=4, max_cycles=120, continuous_actions=False)
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig):
+    assert (
+        cfg.train.active ^ cfg.eval.active
+    ), "Please specify one of train.active=true or eval.active=true in the arguments."
+    env_kwargs = {
+        "num_missing": cfg.env.missing,
+        "num_rescuers": cfg.env.rescuers,
+        "num_trees": cfg.env.trees,
+        "num_safezones": cfg.env.safezones,
+        "max_cycles": cfg.env.max_cycles,
+        "continuous_actions": cfg.env.continuous_actions,
+    }
     env_fn = "search_and_rescue"
-    if args.train:
-        train(env_fn, steps=int(1e5), seed=0, render_mode=None, **env_kwargs)
-    elif args.eval:
-        eval(env_fn, num_games=10, render_mode='human', **env_kwargs)
-    else:
-        print("Please specify either --train or --eval to run the respective process.")
+    if cfg.train.active:
+        train(
+            env_fn,
+            steps=cfg.train.total_timesteps,
+            seed=cfg.train.seed,
+            render_mode=cfg.train.render_mode,
+            **env_kwargs
+        )
+    elif cfg.eval.active:
+        eval(
+            env_fn,
+            num_games=cfg.eval.games,
+            render_mode=cfg.eval.render_mode,
+            **env_kwargs
+        )
 
 
 if __name__ == "__main__":
